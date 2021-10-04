34b7ba3eaea3e22dec8f3e866fafee17db7325a3
mira-kine <mirak0419@gmail.com>
2021-10-04 16:16:29 -0700

    stops game when user is out of guesses

--- app.js
+++ app.js
@@ -13,22 +13,11 @@ const guessCount = document.getElementById('guess-count');
 let randomNum =  Math.floor(Math.random() * (20 - 1 + 1)) + 1;
 let numGuessesRemain = 4;
 
-// what state do i need 
-// what events am I listening for
-//    button Click
-//      Number must be between 1-20 else display "must be 1-20"
-//      decrement the amount of tries by 1
-//      compare the number guessed to the correct guess
-//          if numberGuess < correct guess set to 'too low!'
-//          if numberGuess > correct guess set to 'too high!'
-//          if number Guess = correct guess, go to ending result page "you win"
-//          if number of tries = 0, go to ending result page with "you lose"
-
 playButton.addEventListener ('click', () => {
   randomNum;
   console.log(randomNum);
-  document.getElementById('play-page-id').style.display = "block";
-  document.getElementById('shown-page-id').style.display = "none";
+  document.getElementById('play-page-id').style.display = 'block';
+  document.getElementById('shown-page-id').style.display = 'none';
 });
 
 guessButton.addEventListener ('click', () => {
@@ -46,8 +35,12 @@ guessButton.addEventListener ('click', () => {
   else if (userGuess < randomNum) {
       playerStatus = 'too low!';
   }
-  else if (numGuessesRemain === 0) {
-      document.getElementById('guess-count').style.display = "none";
+  
+  if (numGuessesRemain <= 0) {
+      guessButton.style.display = 'none';
+      document.getElementById('play-page-id').style.display = 'block';
+      document.getElementById('guess-count-footer').style.display = 'block';
+      playerStatus = 'you lost!';
   }
   resultText.textContent = `${playerStatus}`;
   guessCount.textContent = Number(numGuessesRemain);
@@ -65,3 +58,14 @@ guessButton.addEventListener ('click', () => {
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  // what state do i need 
+// what events am I listening for
+//    button Click
+//      Number must be between 1-20 else display "must be 1-20"
+//      decrement the amount of tries by 1
+//      compare the number guessed to the correct guess
+//          if numberGuess < correct guess set to 'too low!'
+//          if numberGuess > correct guess set to 'too high!'
+//          if number Guess = correct guess, go to ending result page "you win"
+//          if number of tries = 0, go to ending result page with "you lose"
